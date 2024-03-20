56d6e986717444031e59bc8165972bd96497fc1c
Deepanshu Verma <108023848+deepanshuv7348@users.noreply.github.com>
2024-03-20 10:31:50 +0530

    Update api.js
    sd

--- dash/src/api/api.js
+++ dash/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const local = 'http://localhost:5000'
-const production = 'https://pack-api.onrender.com'
+const production = 'http://localhost:5000'
 
 let api_url = ''
 let mode = 'pro'
